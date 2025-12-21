15c0ab2d1b5bdf45ec64778f778b93ca1dc454a9
Rubén Campelo <campeloruben@pm.me>
2025-12-21 20:21:51 +0100

    improve reading view

--- lua/setup/mappings.lua
+++ lua/setup/mappings.lua
@@ -1,38 +1,40 @@
+local opt = vim.opt
 -- Options
-vim.o.nu = true -- numbers
-vim.o.rnu = true -- relativenumber
+opt.nu = true -- numbers
+opt.rnu = true -- relativenumber
 
 -- Indentation
-vim.o.et = true -- expandtab
-vim.o.ts = 2 -- tabstop
-vim.o.sw = 2 -- shiftwidth
-vim.o.sr = true -- shiftround round indent to multiple of shiftwidth
-vim.o.si = true -- smartindent
-vim.o.bri = true -- breakindent
-vim.o.list = true
-vim.o.lcs = 'trail:·' -- add dots to spaces at the end of the line
+opt.et = true -- expandtab
+opt.ts = 2 -- tabstop
+opt.sw = 2 -- shiftwidth
+opt.sr = true -- shiftround round indent to multiple of shiftwidth
+opt.si = true -- smartindent
+opt.bri = true -- breakindent
+opt.list = true
+opt.lcs = 'trail:·' -- add dots to spaces at the end of the line
 
 -- Clipboard
-vim.o.cb = 'unnamedplus' -- upgrade clipboard
+opt.cb = 'unnamedplus' -- upgrade clipboard
 
 -- Background
-vim.o.bg = 'light' -- light background
-vim.o.cul = true -- cursorline highlight
+opt.bg = 'light' -- light background
+opt.cul = true -- cursorline highlight
 
 -- Showmode
-vim.o.smd = false --disable last line (mini.statusline)
+opt.smd = false --disable last line (mini.statusline)
 
 -- Quality of life
-vim.o.cf = true -- confirm save before :q
-vim.o.mps = '(:),{:},[:],<:>,«:»' -- highlight pairs
-vim.o.mouse = 'n' -- mouse activated only on normal mode
-vim.o.sb = true -- splitbelow
-vim.o.so = 999 -- keep the cursor line always in the middle
-vim.o.spr = true -- splitright
-vim.o.tgc = true -- termguicolors
-vim.o.tm = 750 -- timeoutlen
-vim.o.ut = 250 -- updatetime
+opt.cf = true -- confirm save before :q
+opt.mps = '(:),{:},[:],<:>,«:»' -- highlight pairs
+opt.lbr = true -- linebreak
+opt.mouse = 'n' -- mouse activated only on normal mode
+opt.sb = true -- splitbelow
+opt.so = 999 -- keep the cursor line always in the middle
+opt.spr = true -- splitright
+opt.tgc = true -- termguicolors
+opt.tm = 750 -- timeoutlen
+opt.ut = 250 -- updatetime
 
 -- History
-vim.o.udf = true -- save history
-vim.o.history = 100 -- how much operations save
+opt.udf = true -- save history
+opt.history = 100 -- how much operations save
